4d351640aceb6ec43e7a4258e7966ba953c390c6
yVbz-Dev <pivatodavi022@gmail.com>
2026-05-03 18:02:16 -0300

    update login route

--- backend/routes/login.go
+++ backend/routes/login.go
@@ -4,22 +4,43 @@ import (
 	"github.com/gofiber/fiber/v2"
 	database "github.com/seu-usuario/vura-backend/db"
 	"github.com/seu-usuario/vura-backend/models"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func InitLogin(app *fiber.App) {
 	app.Post("/login", func(c *fiber.Ctx) error {
-		// get body
+		// get id
 		var user models.User
 		err := c.BodyParser(&user)
+
+		// err handling
 		if err != nil {
 			return c.Status(400).JSON(fiber.Map{
 				"error": "invalid json file",
 			})
 		}
 
-		// checks
-		
+		// get db info
+		var userInfo models.User
+		dbErr := database.DB.First(&userInfo, user.ID)
+		if dbErr.Error != nil {
+			return c.Status(500).JSON(fiber.Map{
+				"error": "error fetching backend data",
+			})
+		}
 
-		return c.Status(200).JSON(user)
+		// checks
+		if bcrypt.CompareHashAndPassword([]byte(userInfo.Password), []byte(user.Password)) != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "wrong password",
+			})
+		}
+		if userInfo.Email != user.Email {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "wrong email",
+			})
+		}
+
+		return c.Status(200).JSON(userInfo)
 	})
 }
